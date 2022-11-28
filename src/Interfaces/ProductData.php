575259bffeb9cb257cd9a82722a1737c29902cf0
Deusagnis <deusagnis@gmail.com>
2022-11-28 18:12:14 +0300

    Update Product Data interface.

--- src/Interfaces/ProductData.php
+++ src/Interfaces/ProductData.php
@@ -6,4 +6,6 @@ interface ProductData
 {
     public function findProducts(array $categories, array $sortBy, int $offset, int $count): ?array;
     public function findMainPageProducts(int $offset, int $count): ?array;
+    public function updateProductsAmounts(array $amountsByIds): ?int;
+    public function findByIds(array $ids): ?array;
 }
