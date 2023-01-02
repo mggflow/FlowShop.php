13d949cd5be1e69b340bb39a47cc496fabaf845e
Deusagnis <deusagnis@gmail.com>
2023-01-02 18:42:00 +0300

    Extend PurchaseData interface.

--- src/Interfaces/PurchaseData.php
+++ src/Interfaces/PurchaseData.php
@@ -7,6 +7,8 @@ interface PurchaseData
     public function createAnyPurchases(array $purchases): ?int;
 
     public function findUserPurchases(int $userId, int $offset, int $count): ?array;
-
+    public function findForOrder(int $orderId): ?array;
+    public function updateWithIds(array $purchases): ?array;
     public function deleteOrderPurchases(int $orderId): ?int;
+    public function deleteById(int $purchaseId): bool;
 }
