b03075cf9cdd6ec2b0a423cbc02a567c2f3d446d
Deusagnis <deusagnis@gmail.com>
2023-01-02 18:41:36 +0300

    Extend Product entity.

--- src/Entities/Product.php
+++ src/Entities/Product.php
@@ -5,4 +5,8 @@ namespace MGGFLOW\FlowShop\Entities;
 class Product
 {
     const DURATION_PERIOD = 60;
+
+    public static function isAmountable(object $product): bool{
+        return $product->amount !== null;
+    }
 }
